de4daf418ef713868652180a88c1b2de149082b3
WardenHD <tomas.bezruchko@gmail.com>
2024-04-22 19:38:17 +0300

    Initial commit

--- Main.java
+++ Main.java
@@ -1,83 +1,38 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    static final Scanner scanner = new Scanner(System.in);
-    static final ArrayList<Menu> chosenDishes = new ArrayList<Menu>();
+    static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     public static void main(String[] args) {
-        while(true) {
-            chooseDish();
-            
-            if(!askIfAll()) break;
-        }
+        int[] team1 = fillArrayRandom(new int[5]);
+        int[] team2 = random.ints(5, 18, 40).toArray();
+
+        printArray("Team 1 players:", team1);
+        printArray("Team 2 players:", team2);
+        System.out.println("Team 1 average: " + calculateAverage(intToDoubleArray(team1)));
+        System.out.println("Team 2 average: " + calculateAverage(intToDoubleArray(team2)));
     }
 
-    private static void chooseDish() {
-        String dish;
-        while(true) {
-            System.out.print("Say chosen dish: ");
-            dish = scanner.next().toUpperCase();
-            try { 
-                Menu chosenDish = Menu.valueOf(dish);
-                System.out.println(chosenDish);
-                System.out.println("Chosen dish: " + chosenDish);
-                chosenDishes.add(chosenDish);
-                break; 
-            }
-            catch(Exception e) { 
-                System.out.println("Sorry, this dish isn't available"); 
-                System.out.print("Available items: ");
-                for(Menu d : Menu.values()) System.out.print(d + ", ");
-                System.out.println(" \nChoose again.\n");
-            }
-        }
+    public static int[] fillArrayRandom(int[] array) {
+        for (int i = 0; i < array.length; i++) array[i] = random.nextInt(18, 40);
+        return array;
     }
 
-    private static boolean askIfAll() {
-        while(true) {
-            System.out.print("Is there anything more (yes/no)?: ");
-            String input = scanner.next().toLowerCase();
-            System.out.println(input);
-            if(input.equals("no")) {
-                System.out.println("This what you have chosen:\n");
-                for (Menu menu : chosenDishes) System.out.println(menu);
-                System.out.println("\n");
-                return false;
-            } 
-            else if(input.equals("yes")) return true;
-            else System.out.println("Did not hear you, repeat please\n"); 
-        }
-    }
-}
-
-// Sorry, didn't used switch :(
-enum Menu {
-    COKE("Coke", 6),
-    WATER("Water", 1),
-    JUICE("Juice", 3),
-    PASTA("Pasta", 10),
-    HAMBURGER("Hamburger", 8),
-    PIZZA("Pizza", 15);
-
-    private String name;
-    private int price;
-
-    Menu(String name, int price) {
-        this.name = name;
-        this.price = price;
+    public static void printArray(String prompt, int[] array) {
+        System.out.println((prompt == null) ? "" : prompt + " " + 
+            Arrays.toString(array).replace('[', '\0').replace(']', '\0'));
     }
 
-    public String getName() {
-        return name;
+    public static double calculateAverage(double... args) {
+        double result = 0;
+        for (double d : args) result += d;
+        return result / args.length;
     }
 
-    public int getPrice() {
-        return price;
-    }
-
-    @Override
-    public String toString() {
-        return name + " $" + (float) price;
+    public static double[] intToDoubleArray(int[] array) {
+        double[] result = new double[array.length];
+        for (int i = 0; i < array.length; i++) result[i] = (double) array[i];
+        return result;
     }
 }
