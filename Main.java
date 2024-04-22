a962af8ade5488302347b39097098834a47cff73
WardenHD <tomas.bezruchko@gmail.com>
2024-04-22 19:41:37 +0300

    Initial commit

--- Main.java
+++ Main.java
@@ -1,29 +1,49 @@
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
-
-
 public class Main {
-    static final ThreadLocalRandom random = ThreadLocalRandom.current();
-    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            boolean guessed = guessNumber();
-            System.out.println((guessed) ? "You won the game" : "You didn't guess the number");
-            if(guessed) break;
-        }
+        @SuppressWarnings("unused") // added to remove warnings
+        BurgerMain burgers = new BurgerMain();
+    }
+}
 
-        System.out.println("Press \"ENTER\" to continue...");
-        scanner.nextLine();
+@SuppressWarnings("unused")
+class BurgerMain {
+    private final Burger standartBurger;
+    private final Burger dietBurger;
+    private final Burger doubleMeatBurger;
 
-        for(int i = 0; i < 100; i++) if(!Integer.toString(i).contains("4") && !Integer.toString(i).contains("9")) System.out.println(i);
+    public BurgerMain() {
+        this.standartBurger = new Burger();
+        this.dietBurger = new Burger(true, 1, true, true, false);
+        this.doubleMeatBurger = new Burger(true, 2, true, true, true);
+    }
+}
+
+@SuppressWarnings("unused")
+class Burger {
+    private final boolean buns;
+    private final int meatAmount;
+    private final boolean cheese;
+    private final boolean herbs;
+    private final boolean mayonnaise;
+
+    public Burger() {
+        this(true, 1, true, true, true);
     }
 
-    public static boolean guessNumber() {
-        System.out.print("Enter the number (0-10): ");
-        int chosenNumber = random.nextInt(0, 10 + 1);
-        boolean guessed = (scanner.nextInt() == chosenNumber);
-        System.out.println("The number was " + chosenNumber);
-        return guessed;
+    public Burger(boolean buns, int meatAmount, boolean cheese, boolean herbs, boolean mayonnaise) {
+        this.buns = buns;
+        this.meatAmount = meatAmount;
+        this.cheese = cheese;
+        this.herbs = herbs;
+        this.mayonnaise = mayonnaise;
+
+        System.out.println("\n---Ingredients of Burger---");
+        System.out.println("  Buns: " + ((buns) ? "Present" : "Not present"));
+        System.out.println("  Meat: " + meatAmount + "x");
+        System.out.println("  Cheese: " + ((cheese) ? "Present" : "Not present"));
+        System.out.println("  Herbs: " + ((herbs) ? "Present" : "Not present"));
+        System.out.println("  Mayonnaise: " + ((mayonnaise) ? "Present" : "Not present"));
+        System.out.println("---------------------------\n");
     }
 }
