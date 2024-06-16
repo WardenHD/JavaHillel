7f9dc1d821120e150e63b337fabfd406a6af8fc7
WardenHD <tomas.bezruchko@gmail.com>
2024-06-16 20:25:09 +0300

    Revert commit

--- src/main/java/com/wardendev/hillelproject/Main.java
+++ src/main/java/com/wardendev/hillelproject/Main.java
@@ -2,6 +2,6 @@ package com.wardendev.hillelproject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Made tests for methods!!!");
+        System.out.println("Connect JUnit Maven Library!");
     }
 }
